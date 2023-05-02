fcaaa9c6cea7db13989d54e755184f32c6e16292
Monthlyaway <wilsonyang315@gmail.com>
2023-05-02 13:38:30 +0800

    modify v2

--- Ch5/Num-Sequence/v2/Header/fibonacci.h
+++ Ch5/Num-Sequence/v2/Header/fibonacci.h
@@ -1,53 +1,88 @@
 #pragma once
 #include "num_sequence.h"
 
+//class Fibonacci : public num_sequence {
+//public:
+//	Fibonacci(size_t beg_pos = 1, size_t len = 1) :
+//		num_sequence(beg_pos, len, &_elems)
+//	{
+//		check_integrity(_beg_pos + _len - 1, _relems.size());
+//	}
+//
+//	unsigned long int back(void) const {
+//		return _relems[_beg_pos + _len - 2];
+//	}
+//	string what_am_i(void) const {
+//		return "Fibonacci";
+//	}
+//
+//protected:
+//
+//private:
+//	static vector<int> _elems;
+//
+//	bool gen_elem_to_pos(size_t pos) {
+//		if (pos <= _relems.size())
+//		{
+//			cout << "Warning: no need to generate sequence to position "
+//				<< pos << endl;
+//			return true;
+//		}
+//		else
+//		{
+//			unsigned long int new_val;
+//			unsigned long int vsize = _relems.size();
+//			if (vsize == 0)
+//			{
+//				_relems.push_back(1);
+//				_relems.push_back(1);
+//			}
+//			else if (vsize == 1)
+//				_relems.push_back(1);
+//
+//			for (unsigned long int ix = _relems.size(); ix < pos; ix++)
+//			{
+//				new_val = _relems[ix - 2] + _relems[ix - 1];
+//				_relems.push_back(new_val);
+//			}
+//			return true;
+//		}
+//	}
+//};
+//
+//vector<int> Fibonacci::_elems;
+
 class Fibonacci : public num_sequence {
 public:
-	Fibonacci(size_t beg_pos = 1, size_t len = 1) :
-		num_sequence(beg_pos, len, &_elems)
-	{
-		check_integrity(_beg_pos + _len - 1, _relems.size());
+	Fibonacci(int len = 1, int beg_pos = 1);
+	Fibonacci(const Fibonacci& rhs);  // copy constructor
+	Fibonacci& operator=(const Fibonacci& rhs);
+	virtual Fibonacci* clone() {
+		// ok: Fibonacci is derived from num_sequence 
+        // the virtual keyword in the derived class is optional 
+		return new Fibonacci(*this);
 	}
 
-	unsigned long int back(void) const {
-		return _relems[_beg_pos + _len - 2];
-	}
-	string what_am_i(void) const {
+	virtual const char* what_am_i() const{
 		return "Fibonacci";
 	}
-
 protected:
-
-private:
+	virtual void gen_elems(int pos) const;
 	static vector<int> _elems;
-
-	bool gen_elem_to_pos(size_t pos) {
-		if (pos <= _relems.size())
-		{
-			cout << "Warning: no need to generate sequence to position "
-				<< pos << endl;
-			return true;
-		}
-		else
-		{
-			unsigned long int new_val;
-			unsigned long int vsize = _relems.size();
-			if (vsize == 0)
-			{
-				_relems.push_back(1);
-				_relems.push_back(1);
-			}
-			else if (vsize == 1)
-				_relems.push_back(1);
-
-			for (unsigned long int ix = _relems.size(); ix < pos; ix++)
-			{
-				new_val = _relems[ix - 2] + _relems[ix - 1];
-				_relems.push_back(new_val);
-			}
-			return true;
-		}
-	}
 };
 
 vector<int> Fibonacci::_elems;
+
+
+inline Fibonacci::Fibonacci(int len, int beg_pos)
+	: num_sequence(len, beg_pos, _elems)
+{}
+inline Fibonacci::Fibonacci(const Fibonacci& rhs)
+	: num_sequence(rhs)
+{}
+Fibonacci& Fibonacci::operator=(const Fibonacci& rhs) {
+	if (this != &rhs) {
+		num_sequence::operator=(rhs);   // assign base class members
+	}
+	return *this;
+}
--- Ch5/Num-Sequence/v2/Header/num_sequence.h
+++ Ch5/Num-Sequence/v2/Header/num_sequence.h
@@ -8,46 +8,79 @@ using namespace std;
 * The base class now has all the information necessary to search and display the 
 * elements of the numeric sequence.
 */
+//class num_sequence {
+//public:
+//	virtual ~num_sequence() {}
+//	virtual string what_am_i() const = 0;
+//	int elem(int pos) {
+//		check_integrity(pos, _relems.size());
+//		if (pos > _relems.size())
+//			gen_elem_to_pos(pos);
+//		return _relems[pos - 1];
+//	}
+//	ostream& print(ostream& os = cout) const {
+//		os << what_am_i() << ": (" << _beg_pos << ", " << _len << ") ";
+//		for (unsigned long int ix = _beg_pos - 1; ix <= _beg_pos + _len - 2; ix++)
+//			os << _relems[ix] << " ";
+//		return os;
+//	}
+//	int size() const { return _len; }
+//	int beg_pos() const { return _beg_pos; }
+//	static size_t max_elems() { return 50; }
+//	int back(void) const {
+//		return _relems[_beg_pos + _len - 2];
+//	}
+//protected:
+//	virtual bool gen_elem_to_pos(size_t pos) = 0;
+//	bool check_integrity(size_t pos, size_t size) {
+//		if (pos < 1 || pos > max_elems()) {
+//			cerr << "Error: " << what_am_i() << ": check_integrity() invalid pos" << endl;
+//			return false;
+//		}
+//		if (pos > size) {
+//			gen_elem_to_pos(pos);
+//			return true;
+//		}
+//	}
+//	num_sequence(size_t bp = 1, size_t len = 1, vector<int>& re = *(new vector<int>()))
+//		: _len(len), _beg_pos(bp), _relems(re) {
+//		check_integrity(_beg_pos + _len - 1, _relems.size());
+//	}
+//	// declare reference to vector, no need to check nullptr
+//	int _len;
+//	int _beg_pos;
+//	vector<int>& _relems;
+//};
+
 class num_sequence {
 public:
 	virtual ~num_sequence() {}
-	virtual string what_am_i() const = 0;
-	int elem(int pos) {
-		check_integrity(pos, _relems.size());
-		if (pos > _relems.size())
-			gen_elem_to_pos(pos);
-		return _relems[pos - 1];
-	}
-	ostream& print(ostream& os = cout) const {
-		os << what_am_i() << ": (" << _beg_pos << ", " << _len << ") ";
-		for (unsigned long int ix = _beg_pos - 1; ix <= _beg_pos + _len - 2; ix++)
-			os << _relems[ix] << " ";
-		return os;
-	}
-	int size() const { return _len; }
+	virtual num_sequence* clone() = 0;
+
+	virtual const char* what_am_i() const = 0;
+	int elem(int pos) const;
+	ostream& print(ostream& os = cout) const;
+	int length() const { return _length; }
 	int beg_pos() const { return _beg_pos; }
-	static size_t max_elems() { return 50; }
-	int back(void) const {
-		return _relems[_beg_pos + _len - 2];
-	}
+	static int max_elems() { return 64; }
 protected:
-	virtual bool gen_elem_to_pos(size_t pos) = 0;
-	bool check_integrity(size_t pos, size_t size) {
-		if (pos < 1 || pos > max_elems()) {
-			cerr << "Error: " << what_am_i() << ": check_integrity() invalid pos" << endl;
-			return false;
+	virtual void gen_elems(int pos) const = 0;
+	bool check_integrity(int pos, int size) const;
+
+	num_sequence(int len, int bp, vector<int>& re)
+		: _length(len), _beg_pos(bp), _relems(re) {}
+	//num_sequence(int len = 1, int bp = 1, vector<int>* pe = nullptr)
+	//	: _length(len), _beg_pos(bp), _relems(*pe) {}
+	num_sequence& operator=(const num_sequence& rhs) {
+		if (this != &rhs) {
+			_length = rhs._length;
+			_beg_pos = rhs._beg_pos;
+			_relems = rhs._relems;
 		}
-		if (pos > size) {
-			gen_elem_to_pos(pos);
-			return true;
-		}
+		return *this;
 	}
-	num_sequence(size_t bp = 1, size_t len = 1, vector<int>& re = *(new vector<int>()))
-		: _len(len), _beg_pos(bp), _relems(re) {
-		check_integrity(_beg_pos + _len - 1, _relems.size());
-	}
-	// declare reference to vector, no need to check nullptr
-	int _len;
+
+	int _length;
 	int _beg_pos;
 	vector<int>& _relems;
 };
